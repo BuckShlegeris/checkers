11a534e2cc68026d41a4a9abe95b96cdcc79c563
Buck Shlegeris <bshlegeris@gmail.com>
2013-12-02 11:47:35 -0800

    Wrote most of the game logic

--- Board.rb
+++ Board.rb
@@ -1,25 +1,26 @@
 require_relative "Piece.rb"
+require "colorize"
 
 class Board
   attr_accessor :grid
 
   def initialize(fill_board = true)
     if fill_board
-      @grid = Array.new(8) { Array.new(8,nil) }
+      @grid = Array.new(8) { Array.new(8, nil) }
       # TODO: this needs to actually put pieces down
     end
   end
 
   def [](pos)
-    x,y = pos
-    raise "invalid index" unless x.between?(0,7) && y.between?(0,7)
+    x, y = pos
+    raise "invalid index" unless x.between?(0, 7) && y.between?(0, 7)
 
     @grid[y][x]
   end
 
   def []=(pos,value)
     x,y = pos
-    raise "invalid index" unless x.between?(0,7) && y.between?(0,7)
+    raise "invalid index" unless x.between?(0, 7) && y.between?(0, 7)
 
     @grid[y][x] = value
   end
@@ -40,15 +41,35 @@ class Board
     puts
   end
 
-  def add_piece(x, y, color)
-    p = Piece.new(x, y, color, self)
-    @grid[y][x] = p
+  def add_piece(pos, color)
+    p = Piece.new(pos, color, self)
+    self[pos] = p
   end
+
+  def dup
+    out = Board.new(false)
+    out.grid = []
+
+    @grid.each do |row|
+      new_row = []
+      row.each do |piece|
+        if piece.nil?
+          new_row << nil
+        else
+          new_row << Piece.new(piece.pos, piece.color, out)
+        end
+      end
+      out.grid << new_row
+    end
+
+    out
+  end
+
 end
 
 b= Board.new
-b.add_piece(3,5,:white)
-b.add_piece(2,4,:black)
+b.add_piece([3,5],:white)
+b.add_piece([2,4],:black)
 b.render
-p b[[3,5]].perform_jump(1,3)
+b[[3,5]].perform_moves([[1,3]])
 b.render
--- Piece.rb
+++ Piece.rb
@@ -1,8 +1,8 @@
 class Piece
-  attr_accessor :x, :y, :color
+  attr_accessor :pos, :color
 
-  def initialize(x, y, color, board)
-    @x, @y, @color = x, y, color
+  def initialize(pos, color, board)
+    @pos, @color = pos, color
 
     if color == :white
       @dy = -1
@@ -18,29 +18,35 @@ class Piece
     @promoted
   end
 
-  def perform_slide(new_x,new_y)
-    return false unless (new_x - @x).abs == 1
+  def perform_slide(new_pos)
+    x, y = @pos
+    new_x, new_y = new_pos
+
+    return false unless (new_x - x).abs == 1
 
     if promoted? # If you're promoted, you can go backwards
-      return false unless (new_y - @y).abs == 1
+      return false unless (new_y - y).abs == 1
     else
-      return false unless @y + @dy == new_y
+      return false unless y + @dy == new_y
     end
 
-    return false unless @board[[new_x, new_y]].nil?
+    return false unless @board[new_pos].nil?
 
-    move!(new_x,new_y)
+    move!(new_pos)
 
-    return true
+    true
   end
 
-  def perform_jump(new_x,new_y)
-    return false unless (new_x - @x).abs == 2
+  def perform_jump(new_pos)
+    x, y = @pos
+    new_x, new_y = new_pos
+
+    return false unless (new_x - x).abs == 2
 
     if promoted? # If you're promoted, you can go backwards
-      return false unless (new_y - @y).abs == 2
+      return false unless (new_y - y).abs == 2
     else
-      return false unless @y + @dy*2 == new_y
+      return false unless y + @dy * 2 == new_y
     end
 
     thing_we_jump_over = @board[[(x+new_x) / 2, (y+new_y) / 2]]
@@ -50,23 +56,69 @@ class Piece
 
     @board[[(x+new_x) / 2, (y+new_y) / 2]] = nil
 
-    move!(new_x,new_y)
+    move!(new_pos)
 
-    return true
+    true
   end
 
-  def move!(new_x, new_y)
-    @board[[x, y]] = nil
-    @board[[new_x, new_y]] = self
-    self.x, self.y = new_x, new_y
+  def move!(new_pos)
+    @board[pos] = nil
+    @board[new_pos] = self
+    self.pos = new_pos
 
     maybe_promote
   end
 
   def maybe_promote
+    y = @pos[1]
     if y == 0 && color == :white || y == 7 && color == :black
       promoted = true
     end
   end
 
+  def perform_moves!(move_sequence)
+    if move_sequence.length == 1
+      if perform_slide(move_sequence.first)
+        return
+      end
+    end
+
+    move_sequence.each do |move|
+      raise InvalidMoveException unless perform_jump(move)
+    end
+  end
+
+  def perform_moves(move_sequence)
+    if valid_move_seq?(move_sequence)
+      perform_moves!(move_sequence)
+    else
+      raise InvalidMoveException
+    end
+  end
+
+  def valid_move_seq?(move_sequence)
+    new_board = @board.dup
+    old_board = @board
+    old_pos = pos.dup
+    old_promoted = promoted?
+
+    @board = new_board
+    begin
+      perform_moves!(move_sequence)
+    rescue InvalidMoveException
+      out = false
+    else
+      out = true
+    end
+
+    @board = old_board
+    @pos = old_pos
+    @promoted = old_promoted
+
+    out
+  end
+end
+
+
+class InvalidMoveException < RuntimeError
 end
